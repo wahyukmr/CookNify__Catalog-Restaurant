decea1cfa2752df489ab834419abee2d5d7ae87f
wahyukmr <maryu9229@gmail.com>
2025-02-18 14:50:32 +0700

    [fix] 🪳testing workflow issue

--- __tests__/e2e/unfavoriteRestaurant.test.js
+++ __tests__/e2e/unfavoriteRestaurant.test.js
@@ -37,7 +37,9 @@ baseTest.describe('Favorite Restaurant Flow', () => {
 
       await itemContainer.scrollIntoViewIfNeeded();
 
-      const restoItems = itemContainer.locator('list-restaurant-items >> visible=true');
+      const restoItems = itemContainer.locator('list-restaurant-items');
+      await expect(restoItems).toBeVisible();
+
       const restoFirstItem = restoItems.first();
       await expect(restoFirstItem).toBeVisible();
 

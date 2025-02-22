914382f254f42ba94515d5f67c4a777ae1efc0a7
wahyukmr <maryu9229@gmail.com>
2025-02-22 18:14:20 +0700

    [fix] 🪳testing workflow issue

--- __tests__/helpers/toggleDetailFavoriteButton.js
+++ __tests__/helpers/toggleDetailFavoriteButton.js
@@ -1,6 +1,5 @@
 export const toggleDetailFavoriteButton = async (page) => {
   const detailFavoriteButton = page.locator('#detailFavoriteBtn');
-  await expect(detailFavoriteButton).toBeVisible();
 
   await detailFavoriteButton.click();
 

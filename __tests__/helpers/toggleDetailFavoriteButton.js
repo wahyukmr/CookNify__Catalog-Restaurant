0531a89ca94ac3cfbad993c119dab9994b4c0433
wahyukmr <maryu9229@gmail.com>
2025-03-01 22:42:54 +0700

    [fix] 🪳testing in firefox issue

--- __tests__/helpers/toggleDetailFavoriteButton.js
+++ __tests__/helpers/toggleDetailFavoriteButton.js
@@ -1,7 +1,10 @@
 export const toggleDetailFavoriteButton = async (page) => {
+  const listRestaurantItemDetail = page.locator('list-restaurant-item-detail');
+  await listRestaurantItemDetail.waitFor({ state: 'visible', timeout: 60000 });
+
   const detailFavoriteButton = page.locator('#detailFavoriteBtn');
 
-  await detailFavoriteButton.waitFor({ state: 'visible', timeout: 60000 });
+  await detailFavoriteButton.scrollIntoViewIfNeeded();
 
   await detailFavoriteButton.click();
 
